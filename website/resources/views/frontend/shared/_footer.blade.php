576eb7e90dba91e764f6b6419f8cf1d70a899e09
Talen <talen@weypro.com>
2019-12-18 10:28:07 +0800

    資料夾階層修正

--- website/resources/views/frontend/shared/_footer.blade.php
+++
@@ -1,3 +0,0 @@
-<footer id="footer">
-    <div class="copyright">2019 ALL RIGHTS RESERVED. &nbsp;<a href="{{ route('main.privacy',app()->getLocale()) }}">Privacy Polity</a>&nbsp;| &nbsp; TERMS AND CONDITIONS.</div>
-</footer>
