0422cd1c13721fa80dbccbf100d69f0a9e58bd56
Talen <talen@weypro.com>
2019-12-26 16:39:50 +0800

    fix

--- resources/views/backend/term/index.blade.php
+++ resources/views/backend/term/index.blade.php
@@ -42,7 +42,7 @@
                         <!-- widget content -->
                         <div class="widget-body">
                             
-                            <form id="form" method="post" class="form-horizontal" enctype="multipart/form-data" action="{{ route('term.index') }}"
+                            <form id="form" method="post" class="form-horizontal" enctype="multipart/form-data" action="{{ route('term') }}"
                             data-bv-message="This value is not valid"
 							data-bv-feedbackicons-valid="glyphicon glyphicon-ok"
 							data-bv-feedbackicons-invalid="glyphicon glyphicon-remove"
