2bef96b41eb6bbeae55a75250a975022a04175e4
Talen <talen@weypro.com>
2020-04-23 13:14:55 +0800

    修正2020 Privacy Policy

--- resources/views/frontend/shared/_footer.blade.php
+++ resources/views/frontend/shared/_footer.blade.php
@@ -1,3 +1,3 @@
 <footer id="footer">
-    <div class="copyright">2019 ALL RIGHTS RESERVED. &nbsp;<a href="{{ route('main.privacy',app()->getLocale()) }}">Privacy Polity</a>&nbsp;| &nbsp; <a href="{{ route('main.term',app()->getLocale()) }}">TERMS AND CONDITIONS.</a></div>
+    <div class="copyright">2020 ALL RIGHTS RESERVED. &nbsp;<a href="{{ route('main.privacy',app()->getLocale()) }}">Privacy Policy</a>&nbsp;| &nbsp; <a href="{{ route('main.term',app()->getLocale()) }}">TERMS AND CONDITIONS.</a></div>
 </footer>
