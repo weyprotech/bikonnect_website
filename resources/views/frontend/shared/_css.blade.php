d2240991300d269d1a7d59e2aae06156272a4ad3
talen52 <talen@sourcetech.com.tw>
2020-08-13 11:06:26 +0800

    前台更新

--- resources/views/frontend/shared/_css.blade.php
+++ resources/views/frontend/shared/_css.blade.php
@@ -1,3 +1,4 @@
 <link rel="icon" type="image/x-icon" href="{{ URL::asset('frontend/images/favicon.ico') }}">
 <link rel="shortcut icon" type="image/x-icon" href="{{ URL::asset('frontend/images/favicon.ico') }}">
 <link rel="stylesheet" href="{{ URL::asset('frontend/styles/main.css').'?v='.strtotime('now') }}">
+<link type="text/css" rel="stylesheet" href="{{ URL::asset('frontend/styles/featherlight.css').'?v='.strtotime('now') }}"/>
